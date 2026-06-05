8ed82b5871bab383fd8a51c09d5f8bc99b15ef0d
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:59 -0500

    xml commenting

--- src/Soenneker.Vercel.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Vercel.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Vercel.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Vercel.OpenApiClient";
 }
